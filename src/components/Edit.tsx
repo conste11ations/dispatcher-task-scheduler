a39b1d411ee99763f1f843b3f5c3dd08374a1da4
Rachel Kat <rachelkat@gmail.com>
2020-07-26 22:59:20 -0400

    input type time

--- src/components/Edit.tsx
+++ src/components/Edit.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Formik } from 'formik';
+import React, { useEffect } from 'react';
+import { Formik, Field, Form, FormikHelpers } from 'formik';
 
 interface t {
   activeEvent: any,
@@ -7,20 +7,31 @@ interface t {
   show: boolean,
 }
 
+interface Values {
+  name: string;
+  startDate: string;
+  endDate: string;
+}
+
+
 const Edit = ({ activeEvent, onCancel, show }: t) => {
   const showHideClassName = show ? "edit display-block" : "edit display-none";
   console.log("edit: ae is", activeEvent)
+
   return (
     <div className={showHideClassName}>
       <h3>Editing event:</h3>
       <p>{activeEvent.title}</p>
       <Formik
-        initialValues={{ name: activeEvent.title }}
-        onSubmit={(values, actions) => {
+        initialValues={{ name: activeEvent.title, startDate: activeEvent.start, endDate: activeEvent.end }}
+        onSubmit={(
+          values: Values,
+          { setSubmitting }: FormikHelpers<Values>
+        ) => {
           setTimeout(() => {
             alert(JSON.stringify(values, null, 2));
-            actions.setSubmitting(false);
-          }, 1000);
+            setSubmitting(false);
+          }, 500);
         }}
       >
         {props => (
@@ -32,16 +43,30 @@ const Edit = ({ activeEvent, onCancel, show }: t) => {
               value={props.values.name}
               name="name"
             />
-            {props.errors.name && <div id="feedback">{props.errors.name}</div>}
-            <div>
-            <button className="button" type="submit">Submit</button>
-            <button className="button" type="button" value="Go back!" onClick={() => onCancel()}>Cancel</button>
-            </div>
+              <input
+                type="time"
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                value={props.values.startDate}
+                name="startDate"
+              />
+              <input
+                type="time"
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                value={props.values.endDate}
+                name="endDate"
+              />
+              {props.errors.name && <div id="feedback">{props.errors.name}</div>}
+              <div>
+                <button className="button" type="submit">Submit</button>
+                <button className="button" type="button" value="Go back!" onClick={() => onCancel()}>Cancel</button>
+              </div>
           </form>
         )}
       </Formik>
     </div>
-  );
+        );
 }
 
 export default Edit;
