538eb083fae759cb894247283661d6809130c5ba
Rachel Kat <rachelkat@gmail.com>
2020-07-29 03:53:47 -0400

    add header to csv string

--- src/helpers/downloadEvents.tsx
+++ src/helpers/downloadEvents.tsx
@@ -17,44 +17,43 @@ export const createCSVFile = (events: Event[], dateRange: number) => {
   // come up with array of date ranges: divideDates()
   // filter only events that have display: show (courtesy of filterEvents)
   // countActivityByDateRange()
-
   // write to obj (JSON) (this fn)
-  // turn into CSV format and write to file (this fn)
+  // turn into CSV format
 
-const date_format = "YYYY/MM/DD";  
-const dateTuples = divideDates(dateRange);
-const filteredEvents = events.filter(e => e.display === true);
+  //write to file (this fn)
 
-let extractArray = []; // [[date1-date2, 2, 2, 1], [date2-date3, 0, 1, 2], ...]
+  const date_format = "YYYY/MM/DD";
+  const dateTuples = divideDates(dateRange);
+  const filteredEvents = events.filter(e => e.display === true);
 
-for (const tuple of dateTuples) {
-  let record = [];
-  record.push(moment(tuple[0]).format(date_format) + " - " + moment(tuple[1]).format(date_format));
-  record.push(countActivityByDateRange(tuple[0], tuple[1], filteredEvents, "Pickup"));
-  record.push(countActivityByDateRange(tuple[0], tuple[1], filteredEvents, "Dropoff"));
-  record.push(countActivityByDateRange(tuple[0], tuple[1], filteredEvents, "Other"));
-  extractArray.push(record);
-}
-// console.log("extractArray", extractArray);
-console.log(ConvertToCSV(extractArray))
+  let extractArray = [];
+
+  for (const tuple of dateTuples) {
+    let record = [];
+    record.push(moment(tuple[0]).format(date_format) + " - " + moment(tuple[1]).format(date_format));
+    record.push(countActivityByDateRange(tuple[0], tuple[1], filteredEvents, "Pickup"));
+    record.push(countActivityByDateRange(tuple[0], tuple[1], filteredEvents, "Dropoff"));
+    record.push(countActivityByDateRange(tuple[0], tuple[1], filteredEvents, "Other"));
+    extractArray.push(record);
+  }
+  console.log(ConvertToCSV(extractArray))
 }
 
 export const ConvertToCSV = (objArray: object[]) => {
   //dont forget to put header
-  var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-  var str = '';
+  const array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+  let str = "Time-Frame, Pickup, Drop-off, Other\r\n";
 
-  for (var i = 0; i < array.length; i++) {
-      var line = '';
-      for (var index in array[i]) {
-          if (line != '') line += ','
+  for (let i = 0; i < array.length; i++) {
+    let line = '';
+    for (const index in array[i]) {
+      if (line != '') line += ','
 
-          line += array[i][index];
-      }
+      line += array[i][index];
+    }
 
-      str += line + '\r\n';
+    str += line + '\r\n';
   }
-  console.log("convertToCSV", str);
   return str;
 }
 
@@ -64,8 +63,8 @@ export const divideDates = (dateRange: number) => {
   // let result: {[k: string]: any} = {};
   let result = [];
 
-  const iterations = Math.floor(365/dateRange);
-  
+  const iterations = Math.floor(365 / dateRange);
+
   for (let i = 0; i <= iterations; i++) {
     const currentDatePlusDateRange = moment(currentDate).add(dateRange, "day").toDate();
     result.push([currentDate, currentDatePlusDateRange]);
